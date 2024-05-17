aa3047ae73372eda34486cb91ca149a89647a1c7
jaehyeok <jaehyeok7633@naver.com>
2024-05-17 12:47:58 +0900

    #2. todo api 할 일 기능 작성 완료

--- src/main/java/com/example/todo/todoapi/service/TodoService.java
+++ src/main/java/com/example/todo/todoapi/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.example.todo.todoapi.service;
 
 import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.request.TodoModifyRequestDTO;
 import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
 import com.example.todo.todoapi.dto.response.TodoListResponseDTO;
 import com.example.todo.todoapi.entity.Todo;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,7 +28,7 @@ public class TodoService {
         log.info("할 일 저장 완료! 제목: {}", requestDTO.getTitle());
         return retrieve();
     }
-    
+
     // 할 일 목록 가져오기
     public TodoListResponseDTO retrieve() throws Exception {
         List<Todo> entityList = todoRepository.findAll();
@@ -39,5 +41,49 @@ public class TodoService {
         return TodoListResponseDTO.builder()
                 .todos(dtoList)
                 .build();
+
     }
+
+    public TodoListResponseDTO delete(final String todoId) throws Exception {
+
+        Todo todo = todoRepository.findById(todoId).orElseThrow(
+                () -> {
+                    log.error("id가 존재하지 않아 삭제에 실패했습니다. - ID: {}", todoId);
+                    throw new RuntimeException("id가 존재하지 않아 삭제에 실패했습니다.");
+                }
+        );
+        todoRepository.deleteById(todoId);
+
+        return retrieve();
+    }
+
+    public TodoListResponseDTO update(final TodoModifyRequestDTO requestDTO) throws Exception {
+        Optional<Todo> targetEntity
+                = todoRepository.findById(requestDTO.getId());
+
+        targetEntity.ifPresent(todo -> {
+            todo.setDone(requestDTO.isDone());
+            todoRepository.save(todo);
+        });
+
+        return retrieve();
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
