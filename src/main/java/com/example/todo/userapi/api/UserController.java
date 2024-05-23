5831e49691d9194261f156b46033c980d3305210
jaehyeok <jaehyeok7633@naver.com>
2024-05-23 11:25:35 +0900

    #12. 카카오 로그인 처리 진행 중(access_token 발급, 사용자 정보 가져오기)

--- src/main/java/com/example/todo/userapi/api/UserController.java
+++ src/main/java/com/example/todo/userapi/api/UserController.java
@@ -146,7 +146,7 @@ public class UserController {
     }
 
     @GetMapping("/kakaologin")
-    public ResponseEntity<?> kakaoLogin(String code) {
+    public ResponseEntity<?> kakaoLogin(String authCode) {
         log.info("/api/auth/kakaoLogin - GET! code: {}", code);
         userService.kakaoService(code);
     }
@@ -185,6 +185,22 @@ public class UserController {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
